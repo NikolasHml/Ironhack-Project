83e327dd2d7fec3bad62480cea6ae75087f02442
HeimLi <127536715+HeimLi@users.noreply.github.com>
2023-04-25 15:32:33 +0200

    Changes2

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const router = require("express").Router()
 const User = require("../models/User.model")
 const bcrypt = require("bcryptjs")
-const Film = require("../models/Film.model")
 const {isLoggedIn} = require("../middleware/route-guard")
 
 router.get("/signup", (req, res, next) => {
@@ -34,7 +33,7 @@ router.get("/login", (req, res, next) => {
     res.render("login")
   })
   
-router.post("/login", (req, res, next) => {
+  router.post("/login", (req, res, next) => {
     const { username, email, password } = req.body
   
     // Find user in database by username
@@ -60,49 +59,27 @@ router.post("/login", (req, res, next) => {
       })
   })
 
-router.get("/home", (req, res, next) => {
+  router.get("/home", isLoggedIn, (req, res, next) => {
     res.render("home")
 })
-
-
-router.get("/profile", (req, res, next) => {
-    res.render("profile")
-})
-
-router.get("/uploadFilm", (req, res, next) => {
-    res.render("uploadFilm")
-})
-
-router.post("/uploadFilm", (req, res, next) => {
-    console.log(req.body)
-    const { title, brand, camera, asa, format, color, blackOrWhite, filter, location, startedFilm, endedFilm  } = req.body
-
-    if (color == undefined && blackOrWhite == undefined) {
-      res.render("uploadFilm", { message: "Color or black/white, one has to know this yo" })
-      return}
-    // else if (color == true && blackOrWhite == true) {
-    //   res.render("uploadFilm", { message: "Color or black/white, you have to choose" })
-    //   return}
-    else {
-      Film.create( { title, brand, camera, asa, format, color, blackOrWhite, filter, location, startedFilm, endedFilm } )
-      .then(() => {
-          res.redirect("/home")
-      })
-      .catch(error => next(error))
-    }
-})
-
-// router.get("/edit", (req, res, next) => {
-
-// })
 
   router.get("/profile", isLoggedIn, (req, res, next) => {
     res.render("profile")
 })
 
-    router.get("/logout", (req, res, next)=> {
+  router.get("/upload", isLoggedIn, (req, res, next) => {
+    res.render("upload")
+})
+
+router.get("/edit", isLoggedIn, (req, res, next) => {
+    res.render("edit")
+})
+
+router.get("/logout", (req, res, next)=> {
     req.session.destroy()
     res.redirect("/login")
-    })
+})
+
+
 
 module.exports = router
